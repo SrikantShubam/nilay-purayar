dc5ed5324ee21099c1fe66312f814f8560464d09
Agency 101 <agency10169@gmail.com>
2025-11-20 18:16:55 +0530

    ex1

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,24 @@
-// import Image from "next/image";
-// import Banner from "./components/Banner/banner";
-// import About from "./components/about";
-// import Work from "./components/Work/Work";
-// import Projects from "./components/Projects/Projects";
-// import Contact from "./components/Contact";
-// import Footer from "./components/footer";
-// export default function Home() {
-//   return (
-//     <>
-// <Banner/>
-// <About/>
-// <Work />
-// <Projects/>
-// <Contact/>
-// <Footer/>
-//     </>
-  
-//   );
-// }
-
-
-
-
-
-
-
-
+import Image from "next/image";
+import Banner from "./components/Banner/banner";
+import About from "./components/about";
+import Work from "./components/Work/Work";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/footer";
 export default function Home() {
   return (
-    <main style={{ padding: 40 }}>
-      <h1>Debug Home</h1>
-      <p>If you see this on Vercel, the router is fine.</p>
-    </main>
+    <>
+<Banner/>
+<About/>
+<Work />
+<Projects/>
+<Contact/>
+<Footer/>
+    </>
+  
   );
 }
+
+
+
+
